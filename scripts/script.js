1a2782b764ae4a202ae16c658530bae15a41bfcf
MAH <mahajilou88@gmail.com>
2025-12-27 00:05:11 +0000

    added language settings bar and modified navbar

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,7 @@ function OpenNav() {
     nav.offsetHeight; // force reflow
 
     nav.style.animation = "slideInNav 0.3s ease-in-out forwards";
-    close.style.animation = "slideInNav 0.2s ease-in-out forwards";
+    close.style.animation = "opacity0to1 0.2s ease-in-out forwards";
 }
 
 function CloseNav() {
@@ -19,18 +19,56 @@ function CloseNav() {
 
     // reset animation
     nav.style.animation = "none";
-    nav.offsetHeight; // force reflow
+    nav.offsetHeight; 
 
     nav.style.animation = "slideOutNav 0.3s ease-in-out forwards";
     close.style.animation = "none";
-    close.offsetHeight; // force reflow
+    close.offsetHeight; 
 
-    close.style.animation = "slideOutNav 0.3s ease-in-out forwards";
+    close.style.animation = "opacity1to0 0.3s ease-in-out forwards";
 
     setTimeout(() => {
         nav.style.display = "none";
         close.style.display = "none";
     }, 300);
 
+
+}
+
+
+function OpenLang() {
+    const langMenu = document.getElementById("languageMenu");
+    const close = document.getElementById("closeLang");
+
+    langMenu.style.display = "grid";
+    close.style.display = "block";
+
+    // reset animation
+    langMenu.style.animation = "none";
+    langMenu.offsetHeight; // force reflow
+
+    langMenu.style.animation = "slideInLang 0.3s ease-in-out forwards";
+    close.style.animation = "opacity0to1 0.2s ease-in-out forwards";
+}
+
+
+function CloseLang() {
+    const langMenu = document.getElementById("languageMenu");
+    const close = document.getElementById("closeLang");
+
+    // reset animation
+    langMenu.style.animation = "none";
+    langMenu.offsetHeight; 
+
+    langMenu.style.animation = "slideOutLang 0.3s ease-in-out forwards";
+    close.style.animation = "none";
+    close.offsetHeight; 
+
+    close.style.animation = "opacity1to0 0.3s ease-in-out forwards";
+
+    setTimeout(() => {
+        langMenu.style.display = "none";
+        close.style.display = "none";
+    }, 300);
 
 }

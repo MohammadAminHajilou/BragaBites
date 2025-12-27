1cf19c2ad139139e07ac8716ef52b2bb12dc82bd
MAH <mahajilou88@gmail.com>
2025-12-27 19:06:28 +0000

    adding home page slideshow

--- scripts/script.js
+++ scripts/script.js
@@ -69,3 +69,40 @@ function CloseLang() {
     }, 300);
 
 }
+
+
+
+
+
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slideShowImage');
+const dots = document.querySelectorAll('.homeTopSlideshowDot');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+        dots[i].classList.toggle('active', i === index);
+    });
+    currentSlide = index;
+}
+
+function SlideShowNext() {
+    let nextIndex = (currentSlide + 1) % slides.length;
+    showSlide(nextIndex);
+}
+
+function SlideShowPrevious() {
+    let prevIndex = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(prevIndex);
+}
+
+function SlideShow(index) {
+    showSlide(index - 1); // index از 1 شروع شده تو HTML
+}
+
+// نمایش اولیه
+showSlide(currentSlide);
+
+// تغییر خودکار هر ۵ ثانیه

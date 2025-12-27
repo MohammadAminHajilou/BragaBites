ccb7a2d0410f96889795cd9a4c93c3f5b512e9df
MAH <mahajilou88@gmail.com>
2025-12-27 15:44:25 +0000

    completing order page

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,9 @@ function OpenNav() {
     nav.style.display = "block";
     close.style.display = "block";
 
-    // reset animation
+
     nav.style.animation = "none";
-    nav.offsetHeight; // force reflow
+    nav.offsetHeight; 
 
     nav.style.animation = "slideInNav 0.3s ease-in-out forwards";
     close.style.animation = "opacity0to1 0.2s ease-in-out forwards";
@@ -17,7 +17,7 @@ function CloseNav() {
     const nav = document.getElementById("navbar");
     const close = document.getElementById("navCloseIcon");
 
-    // reset animation
+
     nav.style.animation = "none";
     nav.offsetHeight; 
 
